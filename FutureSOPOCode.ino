7898d360a32da682376c79c3ab0539b15afebc0f
Shubham Kumar <53854055+codeshubham@users.noreply.github.com>
2020-10-02 01:17:46 +0530

    Update FutureSOPOCode.ino

--- FutureSOPOCode.ino
+++ FutureSOPOCode.ino
@@ -1,39 +1,39 @@
 #include <Servo.h>
 
-Servo servo;
-int light1,light2,a=10,error=65,ip=90;
+Servo s;
+int l1,l2,a=10,error=65,ip=90;
 
 void setup() {
   pinMode(a,OUTPUT);
    pinMode(A1, INPUT);   
   pinMode(A0, INPUT);
-     servo.attach(12);
+     s.attach(12);
 Serial.begin(9600);
-servo.write(90);
+s.write(90);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-light1=analogRead(A1);
-light2=analogRead(A0);
-Serial.println(light1);
-Serial.println(light2);
+l1=analogRead(A1);
+l2=analogRead(A0);
+Serial.println(l1);
+Serial.println(l2);
 
 
-int diff1=abs(light1-light2);
-int diff2=abs(light2-light1);
+int diff1=abs(l1-l2);
+int diff2=abs(l2-l1);
 if((diff1 <= error) || (diff2 <= error)) {
   ip=ip+0;
   } else {    
-    if(light1 > light2)
+    if(l1 > l2)
     {
       ip=ip-1;;  
     }
-    if(light1 < light2) 
+    if(l1 < l2) 
     {
       ip=ip+1; 
     }
   }
- servo.write(ip); 
+ s.write(ip); 
   delay(100);
  }
